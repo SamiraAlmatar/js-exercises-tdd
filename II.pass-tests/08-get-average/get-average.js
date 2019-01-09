ab8852ab555faa2db584c79c525ef88be0323296
samm <sammura.mg@googlemail.com>
2019-01-09 02:58:53 +0000

    array average

--- II.pass-tests/08-get-average/get-average.js
+++ II.pass-tests/08-get-average/get-average.js
@@ -1,3 +1,11 @@
 // the input is an array of numbers and strings
 // return the average of all the numbers
 // be sure to exclude the strings
+function average(array){
+    var numArray = array.filter(x => Number.isInteger(x));
+    var total =0 ;
+    numArray.forEach(x => total += x);
+    return total / numArray.length;
+}
+
+module.exports = average;

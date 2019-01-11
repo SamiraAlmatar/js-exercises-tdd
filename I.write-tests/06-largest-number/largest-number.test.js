81608b9116f90d394b8db6fab4fc6f67a6530574
samm <sammura.mg@googlemail.com>
2019-01-11 23:13:03 +0000

    get the largest number

--- I.write-tests/06-largest-number/largest-number.test.js
+++ I.write-tests/06-largest-number/largest-number.test.js
@@ -3,3 +3,14 @@
 // expected: 88;
 
 // also test that the original array hasn't changed
+var largestNumber = require('./largest-number')
+
+test('get the largest number i the array', function(){
+    var input = [3, 21, 88, 4, 36];
+    var expected = 88;
+
+    var output = largestNumber(input);
+
+    expect(expected).toEqual(expected);
+    expect(input).toEqual(input);
+});

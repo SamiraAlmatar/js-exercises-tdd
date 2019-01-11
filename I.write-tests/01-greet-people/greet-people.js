af49dbb3712f811eb71e171f08a738b672ff54b2
samm <sammura.mg@googlemail.com>
2019-01-11 21:52:37 +0000

    greeting people

--- I.write-tests/01-greet-people/greet-people.js
+++ I.write-tests/01-greet-people/greet-people.js
@@ -1,11 +1,5 @@
 function greetPeople(people) {
-    var greeting = 'Hello ';
-
-    people.forEach(function (person) {
-        greeting = greeting + person;
-    });
-
-    return greeting;
+    return people.map(x => 'Hello ' + x);
 }
 
 module.exports = greetPeople;
--- I.write-tests/01-greet-people/greet-people.test.js
+++ I.write-tests/01-greet-people/greet-people.test.js
@@ -1,7 +1,13 @@
+var greetPeople = require('./greet-people')
 test('print list of names prefixed with Hello', function () {
     // Arrange
-
+    var people = ['Samm', 'Majd', 'Waseem'];
+    var expected = ['Hello Samm',
+                    'Hello Majd',
+                    'Hello Waseem'];
+    //var expected = 'Hello SammMajdWaseem'
     // Act
-
+    var output = greetPeople(people);
     // Assert
-})
+    expect(output).toEqual(expected);
+});
